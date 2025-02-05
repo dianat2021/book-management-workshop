9fe3329bacd408ef33a132173fe7877240cb190e
Reza Dianat <dianat.reza66@gmail.com>
2025-02-05 13:24:50 +0100

    implement form toggle feature

---
+++ src/js/ui.js
@@ -0,0 +1,26 @@
+class Ui {
+  static toggleBookTypeFields(
+    printedBookContainer,
+    audioBookContainer,
+    audioFields,
+    printedFields,
+    bookType
+  ) {
+    console.log("Test");
+
+    // HIDE BOTH CONTAINERS INITIALLY
+    printedBookContainer.style.display = "none";
+    audioBookContainer.style.display = "none";
+    // RESET THE VALUES OF BOTH CATEGORIES
+    printedFields.forEach((field) => (field.value = ""));
+    audioFields.forEach((field) => (field.value = ""));
+    // DISPLAY THE RELEVANT CONTAINER BASED ON USER'S SELECTION
+    if (bookType === "printed-book") {
+      printedBookContainer.style.display = "block";
+    } else {
+      audioBookContainer.style.display = "block";
+    }
+  }
+}
+
+export default Ui;
